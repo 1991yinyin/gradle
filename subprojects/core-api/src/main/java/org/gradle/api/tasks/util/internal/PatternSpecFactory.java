a714e5c781ae40df565af12214983719e4a5f8f3
Cedric Champeau <cedric@gradle.com>
2021-01-19 15:22:02 +0100

    Make changing default excludes at runtime an error
    This commit changes the deprecated behavior of changing the default
    excludes at runtime, to make it an error instead.
    
    Closes #15543

--- subprojects/core-api/src/main/java/org/gradle/api/tasks/util/internal/PatternSpecFactory.java
+++ subprojects/core-api/src/main/java/org/gradle/api/tasks/util/internal/PatternSpecFactory.java
@@ -17,6 +17,7 @@
 package org.gradle.api.tasks.util.internal;
 
 import org.apache.tools.ant.DirectoryScanner;
+import org.gradle.api.InvalidUserCodeException;
 import org.gradle.api.file.FileTreeElement;
 import org.gradle.api.internal.file.RelativePathSpec;
 import org.gradle.api.internal.file.pattern.PatternMatcher;
@@ -24,7 +25,6 @@ import org.gradle.api.internal.file.pattern.PatternMatcherFactory;
 import org.gradle.api.specs.Spec;
 import org.gradle.api.specs.Specs;
 import org.gradle.api.tasks.util.PatternSet;
-import org.gradle.internal.deprecation.DeprecationLogger;
 import org.gradle.internal.service.scopes.Scope;
 import org.gradle.internal.service.scopes.ServiceScope;
 
@@ -98,13 +98,7 @@ public class PatternSpecFactory {
         sortedExcludesFromSettings.sort(Comparator.naturalOrder());
         List<String> sortedNewExcludes = Arrays.asList(newDefaultExcludes);
         sortedNewExcludes.sort(Comparator.naturalOrder());
-        DeprecationLogger
-            .deprecateIndirectUsage("Changing default excludes during the build")
-            .withContext(String.format("Default excludes changed from %s to %s.", sortedExcludesFromSettings, sortedNewExcludes))
-            .withAdvice("Configure default excludes in the settings script instead.")
-            .willBeRemovedInGradle7()
-            .withUpgradeGuideSection(6, "changing_default_excludes_during_the_execution_phase")
-            .nagUser();
+        throw new InvalidUserCodeException(String.format("Cannot change default excludes during the build. They were changed from %s to %s. Configure default excludes in the settings script instead.",  sortedExcludesFromSettings, sortedNewExcludes));
     }
 
     public synchronized void setDefaultExcludesFromSettings(String[] excludesFromSettings) {
